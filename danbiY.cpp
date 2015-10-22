c3d5e057357ec8dc6fecc4f0cccf4a752148f861
dyoun1 <danbiyoun@yahoo.com>
2015-10-22 14:45:16 -0700

    Update danbiY.cpp

--- danbiY.cpp
+++ danbiY.cpp
@@ -1,20 +1,15 @@
 /* Danbi Youn */
 
 #include <iostream>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
-#include <GL/glx.h>
 #include "danbiY.h"
 
 
-
-
-#define rnd() (float)rand() / (float)RAND_MAX
-
-void makeBullet(int x, int y)
+void makeBullet(int ab, int bc)
 {   
-    s.center.x = x;
-    s.center.y = y;
-    velocity.y = rnd() - 0.5;
-    velocity.y = 1.0 + rnd() - 0.5;
+    Bullet *b;
+    b->s.center.x = (float)ab;
+    b->s.center.y = (float)bc;
+    b->velocity.x = 4.0;
+    b->velocity.y = 1.0;
 }
+
